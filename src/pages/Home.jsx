9f72828845997aedaccee50b5402c8cdccd6ffc6
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-03-10 20:53:47 +0530

    Home page here

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../App.css";
 import { RiImageAiFill } from "react-icons/ri";
 import { BsImage } from "react-icons/bs";
 import { BsChatLeft } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa6";
 import { FaArrowUp } from "react-icons/fa6";
+import { dataContext } from "../context/UserContext";
+import Chat from "./Chat";
 
 
 const Home = () => {
+
+  let{startRes, setStartRes} = useContext(dataContext);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    setStartRes(true);
+  }
+
   return (
     <div className="home">
       <nav>
         <div className="logo">Smart Ai Bot</div>
       </nav>
-      <div className="hero">
+
+      {!startRes ? <div className="hero">
         <span id="tag">How can i help with ?</span>
         <div className="cate">
           <div className="upImg">
@@ -29,9 +40,11 @@ const Home = () => {
             <span>Let's chat </span>
           </div>
         </div>
-      </div>
+      </div>: <Chat/>
+      }
+
       
-      <form action="" className="input-box">
+      <form action="" className="input-box" onSubmit={(e)=>handleSubmit(e)}>
         <button id="add"><FaPlus /></button>
         <input type="" placeholder="Ask some questions..." />
         <button id="submit"><FaArrowUp /></button>
